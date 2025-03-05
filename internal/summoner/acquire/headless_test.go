23de701ff9371707a0dfe5b37aac39894039c00b
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-03-05 17:30:40 -0500

    Add a test container for headless (#40)
    * Add a test container for headless
    
    * wait strategy

--- internal/summoner/acquire/headless_test.go
+++ internal/summoner/acquire/headless_test.go
@@ -1,40 +1,88 @@
 package acquire
 
 import (
+	"context"
 	"net/http"
 	"testing"
 	"time"
 
+	"fmt"
+
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-var HEADLESS_URL = "http://127.0.0.1:9222"
+type HeadlessContainer struct {
+	mappedPort int
+	url        string
+	Container  *testcontainers.Container
+}
 
-func PingHeadless() (int, error) {
+// Spin up a local graphdb container and the associated client
+func NewHeadlessContainer() (HeadlessContainer, error) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "chromedp/headless-shell:latest",
+		ExposedPorts: []string{"9222/tcp"},
+		Name:         "gleanerHeadlessTestcontainer",
+		WaitingFor:   wait.ForListeningPort("9222/tcp"),
+	}
+	headlessC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+		Reuse:            true,
+	})
+
+	if err != nil {
+		return HeadlessContainer{}, err
+	}
+	// 9222 is the default http endpoint
+	port, err := headlessC.MappedPort(ctx, "9222/tcp")
+	if err != nil {
+		return HeadlessContainer{}, err
+	}
+
+	host, err := headlessC.Host(ctx)
+
+	if err != nil {
+		return HeadlessContainer{}, err
+	}
+
+	url := "http://" + host + ":" + fmt.Sprint(port.Int())
+
+	return HeadlessContainer{mappedPort: port.Int(), url: url, Container: &headlessC}, nil
+}
+
+func (c *HeadlessContainer) Ping() (int, error) {
+
 	var client = http.Client{
 		Timeout: 2 * time.Second,
 	}
 
-	req, err := http.NewRequest("HEAD", HEADLESS_URL, nil)
+	req, err := http.NewRequest("HEAD", c.url, nil)
 	if err != nil {
 		return 0, err
 	}
+
 	resp, err := client.Do(req)
 	if err != nil {
 		return 0, err
 	}
+
 	resp.Body.Close()
 	return resp.StatusCode, nil
 }
 
 func TestHeadlessNG(t *testing.T) {
-	status, err := PingHeadless()
-	t.Log(status)
+	container, err := NewHeadlessContainer()
+	require.NoError(t, err)
 
-	if err != nil || status != 200 {
-		t.Skip("Skipping headless tests because no headless browser is running.")
-	}
+	status, err := container.Ping()
+	require.NoError(t, err)
+	require.Equal(t, 200, status)
 
 	tests := []struct {
 		name         string
@@ -60,7 +108,7 @@ func TestHeadlessNG(t *testing.T) {
 
 		conf := map[string]interface{}{
 			"minio":    map[string]interface{}{"bucket": "test"},
-			"summoner": map[string]interface{}{"threads": "5", "delay": 10, "headless": HEADLESS_URL},
+			"summoner": map[string]interface{}{"threads": "5", "delay": 10, "headless": container.url},
 			"sources":  []map[string]interface{}{{"name": test.name, "headlessWait": test.headlessWait}},
 		}
 
